cd4b74ebc892b52e8d90fbbe2ffbe7f0261bef07
sowro <sowrovnil59@gmail.com>
2024-03-05 21:16:25 +0600

    item  data

---
+++ app/src/main/java/com/example/al_qurantasbeeh/BlankFragment2.java
@@ -0,0 +1,32 @@
+package com.example.al_qurantasbeeh;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BlankFragment2 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View myview2=inflater.inflate(R.layout.fragment_blank2, container, false);
+        // Inflate the layout for this fragment
+
+
+
+
+
+
+
+
+
+        return myview2;
+    }
+}
